3c27d71d2a1e8ea51f9848c80977d153c625aa10
Shiloli-r <ronaldshiloli@gmail.com>
2022-02-09 13:45:17 +0300

    prediction

--- main.py
+++ main.py
@@ -9,10 +9,23 @@ import numpy as np
 model = VGG16(weights='imagenet')
 
 # model summary
-print(model.summary())
+# print(model.summary())
 
 # loading a sample image
 img_path = "input/dog.jpg"
 
 img = image.load_img(img_path, color_mode='rgb', target_size=(224, 224))
 
+# ===== Resizing to fit into VGGNet ======
+
+# converts a PIL image to 3D Num Array
+x = image.img_to_array(img)
+# Adding the 4th dimension, for number of images
+x = np.expand_dims(x, axis=0)
+
+# ==== Using the pre-trained model for prediction =====
+x = preprocess_input(x)
+features = model.predict(x)
+p = decode_predictions(features)
+
+print(p)
